15c8cddd48a7d9cde1f8621912912e755c173174
vumic <vumi@mail.gvsu.edu>
2020-04-17 14:39:03 -0400

    pls

--- Server.js
+++ Server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+const express = require('express');  
+const app = express();  
+app.use(express.static(__dirname + '/src'));  
+app.all('*', (req, res) => {  
+  res.status(200).sendFile(__dirname + '/src/index.html');  
+});  
+app.listen(process.env.PORT || 8080);  
