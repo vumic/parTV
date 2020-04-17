330f35811bae3a061a47c6b6141a648e5372d3cf
vumic <vumi@mail.gvsu.edu>
2020-04-17 16:47:21 -0400

    try

--- Server.js
+++ Server.js
@@ -1,14 +1,12 @@
 const express = require('express');  
-const path = require('path');
 const app = express();  
-app.use(express.static(`${__dirname}/src`));
-
+app.use(express.static(`${__dirname}/dist/demo`));
 
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
   });
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/demo/index.html'));
   });
 app.listen(process.env.PORT || 8080);  
