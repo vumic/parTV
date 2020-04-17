9468494b509ad1a04fa7d52d4fb22f41c0ea93a5
vumic <vumi@mail.gvsu.edu>
2020-04-17 15:29:33 -0400

    folder

--- Server.js
+++ Server.js
@@ -1,7 +1,5 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/src'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/src/index.html');  
-});  
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 8080);  
